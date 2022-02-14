df4e0c95904a48b05a903aa74cd3ba3c4f852022
leylifrida230 <55417475+leylifrida230@users.noreply.github.com>
2022-02-14 09:57:21 +0700

    master

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use('/member', member)
 app.use('/paket', paket)
 app.use('/users', users)
 app.use('/transaksi', transaksi)
-app.use('/api/auth', login)
+app.use('/auth', login)
 
 app.listen(8000, () => {
     console.log('Server run on port 8000')
